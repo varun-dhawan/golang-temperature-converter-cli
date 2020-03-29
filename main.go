176d25f1bb7b9012bcbf4e6fdae41f5f702ef555
Varun Dhawan <varun.dhawan@target.com>
2020-03-29 02:25:09 -0500

    - fixing should convert again error

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func main() {
 		}
 
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
-		_, err = fmt.Scanln(&originValue)
+		_, err = fmt.Scanln(&shouldConvertAgain)
 		if err != nil {
 			printError(errReadingInput)
 		}
